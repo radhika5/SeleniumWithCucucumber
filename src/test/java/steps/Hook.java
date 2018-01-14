4a6a98581eb112cf35566e442b969d7cba942905
radhika5 <radhika.v@echidnainc.com>
2018-01-14 21:03:15 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "/home/radhika.v/Lib/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/home/radhika.v/Lib/chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
